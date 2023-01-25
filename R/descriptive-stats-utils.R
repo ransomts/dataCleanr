9048a331edca751a28a584f073cf16b93e7f2145
Tim Ransom <tsranso@clemson.edu>
2023-01-24 20:13:58 -0500

    an ontology of version control

---
+++ R/descriptive-stats-utils.R
@@ -0,0 +1,22 @@
+#' compute the normality of a data column with the shapiro test
+#'
+#' @param data Tibble from spreadsheet
+#' @param var The column name in data to look at
+#' @param data_type String, "continuous", "ordinal", "categorical", "binary"
+#' @param output something to do with the shapiro test
+#'
+#' @return return something
+compute_normality <- function(data, var, data_type, output) {
+  if (data_type == "continuous") {
+    test_results <- data %>%
+      dplyr::pull(!!var) %>%
+      shapiro.test()
+    if (output == "p") {
+      return(test_results$p.value)
+    } else if (output == "stat") {
+      return(test_results$statistic)
+    }
+  }
+  return(NA)
+}
+compute_normality <- Vectorize(compute_normality)
---
+++ tests/testthat/test-descriptive-stats-utils.R
@@ -0,0 +1,3 @@
+test_that("multiplication works", {
+  skip("not yet implemented")
+})
